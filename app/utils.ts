ed7c7e0b78bef1b94d0cf5ea8ee60e85ae362c7f
isimmons <isimmons33@gmail.com>
2023-12-14 04:39:36 -0500

    add invariant response function

--- app/utils.ts
+++ app/utils.ts
@@ -74,3 +74,34 @@ export function useUser(): User {
 export function validateEmail(email: unknown): email is string {
   return typeof email === "string" && email.length > 3 && email.includes("@");
 }
+
+/**
+ * Provide a condition and if that condition is falsey, this throws a 400
+ * Response with the given message.
+ *
+ * inspired by invariant from 'tiny-invariant'
+ *
+ * @example
+ * invariantResponse(typeof value === 'string', `value must be a string`)
+ *
+ * @param condition The condition to check
+ * @param message The message to throw
+ * @param responseInit Additional response init options if a response is thrown
+ *
+ * @throws {Response} if condition is falsey
+ */
+export function invariantResponse(
+  condition: unknown,
+  message?: string | (() => string),
+  responseInit?: ResponseInit,
+): asserts condition {
+  if (!condition) {
+    throw new Response(
+      typeof message === "function"
+        ? message()
+        : message ||
+          "An invariant failed, please provide a message to explain why.",
+      { status: 400, ...responseInit },
+    );
+  }
+}
