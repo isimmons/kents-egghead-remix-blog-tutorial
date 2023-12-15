bea693b20a1a98e135da78a516b2d7a9dbc9d0f6
isimmons <isimmons33@gmail.com>
2023-12-15 00:35:31 -0500

    prefetch posts

--- app/routes/posts+/_index.tsx
+++ app/routes/posts+/_index.tsx
@@ -24,7 +24,11 @@ const PostsRoute = () => {
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link to={post.slug} className="text-blue-600 underline">
+            <Link
+              to={post.slug}
+              prefetch="intent"
+              className="text-blue-600 underline"
+            >
               {post.title}
             </Link>
           </li>
